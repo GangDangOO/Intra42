2805739b96dad8591be322e21f6975f7715736f9
Sanghyeok Seo <saseo@c4r7s6.42seoul.kr>
2020-07-15 11:51:07 +0900

    C00 finish

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -1,29 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: saseo <saseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/13 21:58:03 by saseo             #+#    #+#             */
-/*   Updated: 2020/07/15 10:30:49 by saseo            ###   ########.fr       */
+/*   Created: 2020/07/15 10:31:36 by saseo             #+#    #+#             */
+/*   Updated: 2020/07/15 11:50:25 by saseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putnbr(int nb)
+void	ft_while(int i, int num, int a, char c[10])
 {
-	char c;
+	int j;
+	int n;
 
-	if (nb < 10)
+	if (i == a)
 	{
-		c = nb + 48;
-		write(1, &c, 1);
+		n = 0;
+		while (n < a)
+		{
+			write(1, &c[n], 1);
+			n++;
+		}
+		if (!(c[0] == '0' + 10 - n))
+			write(1, ", ", 2);
 		return ;
 	}
-	ft_putnbr(nb / 10);
-	nb %= 10;
-	c = nb + 48;
-	write(1, &c, 1);
+	j = num;
+	while (j < 10)
+	{
+		c[i] = j + 48;
+		ft_while(i + 1, j + 1, a, c);
+		j++;
+	}
+}
+
+void	ft_print_combn(int a)
+{
+	char	c[10];
+	int		i;
+	int		num;
+
+	i = 0;
+	num = 0;
+	ft_while(i, num, a, c);
 }
