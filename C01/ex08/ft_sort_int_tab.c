0ad20ac08a53d0f63e6195a497900b8bfab31c33
Sanghyeok Seo <saseo@c4r7s6.42seoul.kr>
2020-07-15 19:29:28 +0900

    C01

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saseo <saseo@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/15 16:55:59 by saseo             #+#    #+#             */
+/*   Updated: 2020/07/15 19:28:49 by saseo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int min;
+	int i;
+	int j;
+
+	i = 0;
+	while (i < size)
+	{
+		min = tab[i];
+		j = i;
+		while (j < size)
+		{
+			if (min > tab[j])
+			{
+				min = tab[j];
+				tab[j] = tab[i];
+				tab[i] = min;
+				j = i;
+			}
+			else
+			{
+				j++;
+			}
+		}
+		i++;
+	}
+}
--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,12 +1,1 @@
-cat /etc/passwd \
-	| grep -v \# \
-	| sed 'n; d' \
-	| cut -f 1 -d ':' \
-	| rev \
-	| sort -r \
-	| awk -v S=$FT_LINE1 -v E=$FT_LINE2 'NR >= S && NR <= E' \
-	| tr '\n' ' ' \
-	| sed 's+ +, +g' \
-	| rev | cut -c 3- | rev \
-	| sed 's/$/./' \
-	| tr -d '\n'
+cat /etc/passwd | grep -v \# | sed 'n; d' | cut -f 1 -d ':'	| rev | sort -r	| awk -v S=$FT_LINE1 -v E=$FT_LINE2 'NR >= S && NR <= E' | tr '\n' ' ' | sed 's+ +, +g'	| rev | cut -c 3- | rev	| sed 's/$/./' | tr -d '\n'
