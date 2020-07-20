678f9c41f596533f0094cb57ee91e7e534688e62
Sanghyeok Seo <saseo@c3r7s1.42seoul.kr>
2020-07-20 14:06:12 +0900

    C02

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,18 +6,17 @@
 /*   By: saseo <saseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/18 18:02:07 by saseo             #+#    #+#             */
-/*   Updated: 2020/07/18 20:41:11 by saseo            ###   ########.fr       */
+/*   Updated: 2020/07/20 14:02:15 by saseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_change_char_print(char *str, int n)
+void	ft_change_char(char *str, int n, char hex[20])
 {
-	char hex[20];
-	char c;
-	int i;
-	int mod;
+	unsigned char	c;
+	int				i;
+	int				mod;
 
 	c = str[n];
 	i = 0;
@@ -25,14 +24,24 @@ void	ft_change_char_print(char *str, int n)
 	{
 		mod = c % 16;
 		if (mod < 10)
-			hex[i] = 48 + mod;
+			hex[i] = '0' + mod;
 		else
-			hex[i] = 65 + (mod - 10);
+			hex[i] = 'a' + (mod - 10);
 		c /= 16;
 		i++;
 		if (c == 0)
-			break;
+		{
+			hex[i] = '0';
+			break ;
+		}
 	}
+}
+
+void	ft_print(char *str, int n)
+{
+	char hex[20];
+
+	ft_change_char(str, n, hex);
 	write(1, "\\", 1);
 	write(1, &hex[1], 1);
 	write(1, &hex[0], 1);
@@ -46,7 +55,7 @@ void	ft_putstr_non_printable(char *str)
 	while (str[i])
 	{
 		if (!(str[i] >= ' ' && str[i] <= '~'))
-			ft_change_char_print(str, i);
+			ft_print(str, i);
 		else
 			write(1, &str[i], 1);
 		i++;
