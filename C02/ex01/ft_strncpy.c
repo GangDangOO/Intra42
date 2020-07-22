a7432eb6d0d48753a74b339e3926a450968ba68b
Sanghyeok Seo <saseo@c6r10s6.42seoul.kr>
2020-07-22 21:11:59 +0900

    C02

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: saseo <saseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/15 20:24:37 by saseo             #+#    #+#             */
-/*   Updated: 2020/07/20 17:18:04 by saseo            ###   ########.fr       */
+/*   Updated: 2020/07/22 21:11:38 by saseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	i = 0;
 	while (i < n)
 	{
-		dest[i] = src[i];
 		if (src[i] == '\0')
+		{
+			dest[i] = src[i];
 			break ;
+		}
+		dest[i] = src[i];
 		i++;
 	}
 	return (dest);
